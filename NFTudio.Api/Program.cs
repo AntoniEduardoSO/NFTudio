775690cceef6bbac03f66114be8d2d5d86be5f1a
Antônio Eduardo <=>
2025-05-19 14:35:42 -0300

    Testando

--- NFTudio.Api/Program.cs
+++ NFTudio.Api/Program.cs
@@ -1,6 +1,7 @@
 using NFTudio.Api;
 using NFTudio.Api.Common;
 using NFTudio.Api.Endpoints;
+using Microsoft.AspNetCore.HttpOverrides;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,8 +18,18 @@ var app = builder.Build();
 
 app.ConfigureDevEnvironment();
 
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost,
+    // Render usa IPs dinâmicos, então limpe as listas para confiar em todos
+    KnownNetworks = { },  // vazio
+    KnownProxies  = { }   // vazio
+});
+
+
 app.SeedSql();
 await app.SeedUsersAsync();
+app.UseHttpsRedirection();
 app.UseCors(ApiConfiguration.CorsPolicyName);
 app.UseSecurity();
 app.MapEndpoints();
